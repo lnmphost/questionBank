42720fbfb9f63cda5001fa6b4f5fd530307b85f6
lnmphost <4923001+lnmphost@user.noreply.gitee.com>
2025-07-27 21:51:44 +0800

    添加基础第十九题

---
+++ 1.C语言/基础100题/019/main.c
@@ -0,0 +1,39 @@
+/*
+19.编程找出1000以内的所有完数
+
+题目：一个数如果恰好等于它的因子之和，这个数就称为"完数"。例如6=1＋2＋3.编程找出1000以内的所有完数。
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N 1000
+
+int main()
+{
+	int i = 0, j = 0, k = 0, n = 0, sum = 0;
+	int a[256];
+
+	for (i = 2; i <= N; i++)
+	{
+		sum = a[0] = 1;
+		k = 0;
+		for (j = 2; j <= (i / 2); j++)
+		{
+			if (i % j == 0)
+			{
+				sum += j;
+				a[++k] = j;
+			}
+		}
+		if (i == sum)
+		{
+			printf("%d = %d",i,a[0]);
+			for (n = 1; n <= k; n++)
+				printf(" + %d",a[n]);
+			printf("\n");
+		}
+	}
+
+	system("pause");
+	return 0;
+}
