e1a5853cb8e313c2a77ac3a08571e9529f9d9f9b
lnmphost <4923001+lnmphost@user.noreply.gitee.com>
2025-08-10 14:29:52 +0800

    添加第二十三基础题，打印菱形

---
+++ 1.C语言/基础100题/023/main.c
@@ -0,0 +1,52 @@
+/*
+23.打印菱形
+
+题目：打印出如下图案（菱形）。
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+程序分析：先把图形分成两部分来看待，前四行一个规律，后三行一个规律，利用双重for循环，第一层控制行，第二层控制列。
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i = 0, j = 0, k = 0;
+
+	for (i = 0; i <= 3; i++)
+	{
+		for (j = 0; j <= 2 - i; j++)
+		{
+			printf(" ");
+		}
+
+		for (k = 0; k <= 2 * i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			printf(" ");
+		}
+		for(k = 0;k <= 4 - 2 * i; k++)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
